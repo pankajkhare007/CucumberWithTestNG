b6a7a64b035aa3e7b9fe17462fc641471c284b90
Pankaj Khare <pankaj.0405@gmail.com>
2022-09-18 16:44:01 +0530

    added 2 steps in feature Demo2

--- src/test/java/runner/TestLocalRunner.java
+++ src/test/java/runner/TestLocalRunner.java
@@ -9,8 +9,8 @@ import org.testng.annotations.Listeners;
 
 
 @CucumberOptions(glue={"demo.stepDefinitions"},
-        features = {"src/test/resources/Features"},
-        plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
+        features = {"src/test/resources/Features"}
+       // plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
         //tags = ("@Regression")
 
 )
